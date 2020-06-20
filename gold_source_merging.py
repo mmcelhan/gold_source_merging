e388f09a8b37c353115776173a3231b8ffee3a22
mmcelhan <matthewmcelhaney@gmail.com>
2020-06-20 16:20:58 -0600

    gold source mapping

--- gold_source_merging.py
+++ gold_source_merging.py
@@ -4,12 +4,30 @@ from fuzzywuzzy import process
 import json
 import os
 
+pd.options.mode.chained_assignment = None  # default='warn'
+
 def format_match_string(string):
     """ function that converts to lower case and removes common words """
     string = string.lower().split()
     string = [word for word in string if word not in common_words]
     string = ' '.join(string)
     return string
+
+
+def return_matching_dictionary(right_df, left_df, key, threshold=80):
+    key = key[0]
+    right_df = right_df.loc[right_df['not_matched'] == 0]  # keep the  matched columns
+    right_df.rename(columns={key: 'original_key'}, inplace=True)
+
+    left_df.loc[:, 'value_match'] = left_df.match.map(lambda x: x[0][0] if x[0][1] >= threshold else "")
+    left_df = left_df[[key, 'value_match']]
+
+    left_df = left_df.merge(right_df, how='inner', left_on='value_match', right_on='match_key')
+
+    left_df = left_df[['original_key', key]]
+    matching_dict = left_df.set_index('original_key')[key].to_dict()
+
+    return matching_dict
 
 
 def golden_source_merge(df_list, key, threshold=80, limit=1):
@@ -23,6 +41,8 @@ def golden_source_merge(df_list, key, threshold=80, limit=1):
     """
 
     # create null match columns
+
+    matching_dict = {}
 
     df_1 = df_list.pop(0)
     df_1 = df_1[key]  # drop all other columns
@@ -57,15 +77,16 @@ def golden_source_merge(df_list, key, threshold=80, limit=1):
                                     process.extract(x, s, limit=limit, scorer=fuzzywuzzy.fuzz.token_sort_ratio))
         df_1['match'] = m
 
-        print(df_1)
 
-        not_matched_right_side = df_2['match_key'].apply(lambda x:
+        df_2_matches = df_2['match_key'].apply(lambda x:
                                     process.extract(x, r, limit=limit, scorer=fuzzywuzzy.fuzz.token_sort_ratio))\
                                     .apply(lambda x: [i[1] for i in x if i[1] < threshold])\
                                     .apply(lambda x: 1 if x else 0)  # 0 if empty list
 
+        df_2 = df_2.merge(df_2_matches.rename('not_matched'), left_index=True, right_index=True)
 
-        df_2 = df_2.merge(not_matched_right_side.rename('not_matched'), left_index=True, right_index=True)
+        matching_dict.update(return_matching_dictionary(df_2, df_1, key, threshold))
+
         df_2 = df_2.loc[df_2['not_matched'] == 1]
 
         # drop the score value and only keep the match words
@@ -83,20 +104,20 @@ def golden_source_merge(df_list, key, threshold=80, limit=1):
 
         df_1 = pd.concat([df_1, df_2]).drop_duplicates(key)
 
-
-
         # drop the matching name columns since this is a left join
         df_1 = df_1[df_1.columns.drop(list(df_1.filter(regex='_y')))]
         df_1 = df_1[df_1.columns.drop(['match_key', 'match', 'not_matched'])]
         df_1.reset_index(drop=True, inplace=True)
 
-    return df_1
+    return df_1, matching_dict
 
 
 local_path = os.path.dirname(os.path.abspath(__file__))
 f = open(os.path.join(local_path, "common_words.json"))
 words = json.load(f)
 common_words = words['words']
+
+"""
 
 # Example code
 
@@ -106,7 +127,10 @@ third_df = pd.read_csv('data_3.csv')
 
 df_list = [first_df, second_df, third_df]
 
-new_df = golden_source_merge(df_list, key=['name'])
+new_df, matching_dict = golden_source_merge(df_list, key=['name'])
+
+print(matching_dict)
 # check the results
-print(new_df)
+#print(new_df)
 new_df.to_csv('output.csv', index=False)
+"""
